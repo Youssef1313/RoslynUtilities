c189dc4769dc718b417a2027cc3009e03c1320fa
Youssef1313 <youssefvictor00@gmail.com>
2022-01-29 16:17:33 +0200

    Add project files.

---
+++ RoslynUtilities/Generators/GeneratorWriter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Composition;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.VisualStudio.Composition;
+using MS.CA.Utilities.Generators;
+
+
+// ExportProviderFactory, MefV1HostServices, https://github.com/dotnet/roslyn/issues/34994
+
+//var x = new Lazy<IExportProviderFactory>(
+//                () =>
+//                {
+//                    var discovery = new AttributedPartDiscovery(Resolver.DefaultInstance, isNonPublicSupported: true);
+//                    var parts = Task.Run(() => discovery.CreatePartsAsync(typeof(IGeneratorWriter).Assembly)).GetAwaiter().GetResult();
+//                    var catalog = ComposableCatalog.Create(Resolver.DefaultInstance).AddParts(parts);
+//                    var configuration = CompositionConfiguration.Create(catalog);
+//                    var runtimeComposition = RuntimeComposition.CreateRuntimeComposition(configuration);
+//                    return runtimeComposition.CreateExportProviderFactory();
+//                },
+//                LazyThreadSafetyMode.ExecutionAndPublication);
+
+//var exports = x.Value.CreateExportProvider().GetExports<IGeneratorWriter>();
+
+Console.WriteLine();
+
+
+namespace MS.CA.Utilities.Generators
+{
+    public interface IGeneratorWriter
+    {
+
+    }
+
+    [Export(typeof(IGeneratorWriter))]
+    public class GeneratorWriter : IGeneratorWriter
+    {
+        private readonly GeneratorWriterOptions _options;
+        private int _indentationLevel;
+
+        public GeneratorWriter(GeneratorWriterOptions options)
+        {
+            _options = options;
+        }
+
+        public GeneratorWriter(CSharpCompilation compilation)
+        {
+            _options = new GeneratorWriterOptions
+            {
+                UseFileScopedNamespaces = compilation.LanguageVersion >= LanguageVersionEx.CSharp10,
+                UseTabsForIndentation = false,
+            };
+        }
+
+        private string GetIndentation()
+        {
+            if (_options.UseTabsForIndentation)
+            {
+                return new string('\t', _indentationLevel);
+            }
+
+            return new string(' ', _indentationLevel * 4);
+        }
+    }
+
+    public class GeneratorWriterOptions
+    {
+        /// <summary>
+        /// Determines whether <see cref="GeneratorWriter"/> will use file-scoped namespaces.
+        /// The default is <see langword="false"/> (use block namespaces).
+        /// </summary>
+        public bool UseFileScopedNamespaces { get; init; }
+
+        /// <summary>
+        /// Determines whether <see cref="GeneratorWriter"/> will use tabs for indentation.
+        /// The default is <see langword="false"/> (use spaces).
+        /// </summary>
+        public bool UseTabsForIndentation { get; init; }
+    }
+}
---
+++ RoslynUtilities/Generators/LanguageVersionEx.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace MS.CA.Utilities.Generators
+{
+    internal class LanguageVersionEx
+    {
+        /// <summary>
+        /// C# language version 9.0
+        /// </summary>
+        /// <remarks>
+        /// <para>Features:</para>
+        /// <list type="bullet">
+        /// <item><description>Records</description></item>
+        /// <item><description>Init only setters</description></item>
+        /// <item><description>Top-level statements</description></item>
+        /// <item><description>Pattern matching enhancements</description></item>
+        /// <item><description>Native sized integers</description></item>
+        /// <item><description>Function pointers</description></item>
+        /// <item><description>Suppress emitting localsinit flag</description></item>
+        /// <item><description>Target-typed new expressions</description></item>
+        /// <item><description>Static anonymous functions</description></item>
+        /// <item><description>Target-typed conditional expressions</description></item>
+        /// <item><description>Covariant return types</description></item>
+        /// <item><description>Extension GetEnumerator support for foreach loops</description></item>
+        /// <item><description>Lambda discard parameters</description></item>
+        /// <item><description>Attributes on local functions</description></item>
+        /// <item><description>Module initializers</description></item>
+        /// <item><description>New features for partial methods</description></item>
+        /// </list>
+        /// </remarks>
+        public static LanguageVersion CSharp9 = (LanguageVersion)900;
+
+        /// <summary>
+        /// C# language version 10.0
+        /// </summary>
+        /// <remarks>
+        /// <para>Features:</para>
+        /// <list type="bullet">
+        /// <item><description>Record structs</description></item>
+        /// <item><description>Global using directives</description></item>
+        /// <item><description>Lambda improvements</description></item>
+        /// <item><description>Improved definite assignment</description></item>
+        /// <item><description>Constant interpolated strings</description></item>
+        /// <item><description>Mix declarations and variables in deconstruction</description></item>
+        /// <item><description>Extended property patterns</description></item>
+        /// <item><description>Sealed record ToString</description></item>
+        /// <item><description>Source Generator v2 APIs</description></item>
+        /// <item><description>Method-level AsyncMethodBuilder</description></item>
+        /// </list>
+        /// </remarks>
+        public static LanguageVersion CSharp10 = (LanguageVersion)1000;
+    }
+}
