2b649a8a3a3db0d6c2910097104873baa5f5c368
Youssef Victor <youssefvictor00@gmail.com>
2022-07-13 16:42:32 +0200

    Write named types correctly (#16)
    * Write named types correctly
    
    * Cleanup
    
    * Update build.yml
    
    * Update
    
    * Update build.yml

--- src/MS.CA.Utilities.CSharp/Generators/NamedTypeWriter.cs
+++ src/MS.CA.Utilities.CSharp/Generators/NamedTypeWriter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using MS.CA.Utilities.Generators;
 
 namespace MS.CA.Utilities.CSharp.Generators
@@ -7,6 +10,27 @@ namespace MS.CA.Utilities.CSharp.Generators
     {
         private readonly IGeneratorWriter _generatorWriter;
         private readonly DisposableWriter? _parentWriter;
+
+        private static readonly SymbolDisplayFormat s_format = new SymbolDisplayFormat(
+            globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Omitted,
+            genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters | SymbolDisplayGenericsOptions.IncludeVariance,
+            memberOptions:
+                SymbolDisplayMemberOptions.IncludeParameters |
+                SymbolDisplayMemberOptions.IncludeType |
+                SymbolDisplayMemberOptions.IncludeRef |
+                SymbolDisplayMemberOptions.IncludeContainingType,
+            kindOptions:
+                SymbolDisplayKindOptions.IncludeMemberKeyword,
+            parameterOptions:
+                SymbolDisplayParameterOptions.IncludeName |
+                SymbolDisplayParameterOptions.IncludeType |
+                SymbolDisplayParameterOptions.IncludeParamsRefOut |
+                SymbolDisplayParameterOptions.IncludeDefaultValue,
+            localOptions: SymbolDisplayLocalOptions.IncludeType,
+            miscellaneousOptions:
+                SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers |
+                SymbolDisplayMiscellaneousOptions.UseSpecialTypes |
+                SymbolDisplayMiscellaneousOptions.IncludeNullableReferenceTypeModifier);
 
         public NamedTypeWriter(IGeneratorWriter generatorWriter, INamedTypeSymbol symbol, bool includeContainingSymbol) : base(generatorWriter, symbol)
         {
@@ -30,7 +54,41 @@ namespace MS.CA.Utilities.CSharp.Generators
             string? parentResult = _parentWriter?.GetStartText();
             string indentation = _generatorWriter.GetIndentation();
             _generatorWriter.IncreaseIndentationLevel();
-            return $"{parentResult}{indentation}partial class {Symbol.Name}\r\n{indentation}{{\r\n";
+            return $"{parentResult}{indentation}{GetNamedTypeHeader(Symbol)}\r\n{indentation}{{\r\n";
+        }
+
+        private static string GetNamedTypeHeader(INamedTypeSymbol namedType)
+        {
+            if (namedType.TypeKind is not (TypeKind.Class or TypeKind.Struct or TypeKind.Interface))
+            {
+                throw new ArgumentException("Only classes and structs are the supported named types.");
+            }
+
+            bool isRecord = IsRecord(namedType);
+            string typeKind = (namedType.TypeKind, isRecord) switch
+            {
+                (TypeKind.Class, true) => "record",
+                (TypeKind.Class, false) => "class",
+                (TypeKind.Struct, true) => "record struct",
+                (TypeKind.Struct, false) => "struct",
+                (TypeKind.Interface, false) => "interface",
+                (TypeKind.Interface, true) => throw new InvalidOperationException("TypeKind.Interface cannot be a record."),
+                _ => throw new InvalidOperationException($"Unexpected (TypeKind, IsRecord) pair ('{namedType.TypeKind}', '{isRecord}')."),
+            };
+
+
+            return $"partial {typeKind} {namedType.ToDisplayString(s_format)}";
+        }
+
+        private static bool IsRecord(INamedTypeSymbol namedType)
+        {
+            // TODO:
+            // #if V_3_9_OR_GREATER
+            //     return namedType.IsRecord;
+            // #else
+            //     return false;
+            // #end if
+            return namedType.IsRecord;
         }
     }
 }
